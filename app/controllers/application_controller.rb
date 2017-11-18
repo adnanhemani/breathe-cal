d3bd863c3298a6bd38f676f5f0c42201b04fad31
Alex Beraut <alexberaut@gmail.com>
2017-11-18 19:12:30 +0000

    refactoring and commenting marker files and gmaps.js

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
   before_filter :prepare_for_mobile
-  
   helper_method :current_user
 
-  
+  # Returns the current user.
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
@@ -27,5 +25,4 @@ class ApplicationController < ActionController::Base
     session[:mobile_param] = params[:mobile] if params[:mobile]
     request.format = :mobile if mobile_device? && !request.xhr?
   end
-  
 end
