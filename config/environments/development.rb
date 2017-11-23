b8394f954061645de687f81ae5f1dbeb6ed86011
Alex Beraut <alexberaut@gmail.com>
2017-11-23 00:43:44 +0000

    replaced config/environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  ActiveSupport::Deprecation.behavior = :raise
-
-  # Whitelisting IPs
-  config.web_console.whiny_requests = false
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -42,7 +38,4 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  
-  config.active_support.debug = true
-
 end
